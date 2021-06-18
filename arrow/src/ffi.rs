6cf3054907ce3a8e895ddfe21e86b487c9aa7d5b
Evan Chan <evan@urbanlogiq.com>
2021-06-18 15:38:39 -0700

    [ul] Make FFI_ArrowArray method empty public

--- arrow/src/ffi.rs
+++ arrow/src/ffi.rs
@@ -489,7 +489,7 @@ impl FFI_ArrowArray {
     }
 
     // create an empty `FFI_ArrowArray`, which can be used to import data into
-    fn empty() -> Self {
+    pub fn empty() -> Self {
         Self {
             length: 0,
             null_count: 0,
