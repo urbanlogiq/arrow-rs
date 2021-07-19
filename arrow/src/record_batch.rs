f6cebab3a1565de82d35e79126295f974900a3d4
Evan Chan <evan@urbanlogiq.com>
2021-07-19 16:01:05 -0700

    Revert "UL: dump backtrace during record_batch validation (XXX - debug)"
    This reverts commit e946122a2eeaf8dddcaabc0f5a0e45cfc3b3e718.

--- arrow/src/record_batch.rs
+++ arrow/src/record_batch.rs
@@ -24,11 +24,6 @@ use crate::array::*;
 use crate::datatypes::*;
 use crate::error::{ArrowError, Result};
 
-fn dump_backtrace() {
-    let bt = backtrace::Backtrace::new();
-    println!("XXX: backtrace = {:?}", bt);
-}
-
 /// A two-dimensional batch of column-oriented data with a defined
 /// [schema](crate::datatypes::Schema).
 ///
@@ -143,7 +138,6 @@ impl RecordBatch {
                     ));
                 }
                 if column.data_type() != schema.field(i).data_type() {
-                    dump_backtrace();
                     return Err(ArrowError::InvalidArgumentError(format!(
                         "column types must match schema types, expected {:?} but found {:?} at column index {}",
                         schema.field(i).data_type(),
@@ -163,7 +157,6 @@ impl RecordBatch {
                     .data_type()
                     .equals_datatype(schema.field(i).data_type())
                 {
-                    dump_backtrace();
                     return Err(ArrowError::InvalidArgumentError(format!(
                         "column types must match schema types, expected {:?} but found {:?} at column index {}",
                         schema.field(i).data_type(),
